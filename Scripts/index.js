65523e934018c24c40f5edc66aa8c5291c845950
roi-e <40072297+roi-e@users.noreply.github.com>
2021-07-06 13:50:02 +0300

    added bunch of stuff

---
+++ Scripts/index.js
@@ -0,0 +1,36 @@
+var ball = document.getElementById("ball");
+
+ball.onmousedown = function (event) {
+  let shiftX = event.clientX - ball.getBoundingClientRect().left;
+  let shiftY = event.clientY - ball.getBoundingClientRect().top;
+
+  ball.style.position = "absolute";
+  ball.style.zIndex = 1000;
+  document.body.append(ball);
+
+  moveAt(event.pageX, event.pageY);
+
+  // moves the ball at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX - shiftX + "px";
+    ball.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the ball on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the ball, remove unneeded handlers
+  ball.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    ball.onmouseup = null;
+  };
+};
+
+ball.ondragstart = function () {
+  return false;
+};
