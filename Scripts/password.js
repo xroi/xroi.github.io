7e460176f425a8d7780cf935cd8814c65ee58636
xroi <40072297+xroi@users.noreply.github.com>
2022-03-17 18:30:36 +0200

    234234

--- Scripts/password.js
+++ Scripts/password.js
@@ -1,5 +1,10 @@
 function fn1() {
-    var str = document.getElementById('text');
-    alert("value is: " + str);
-    document.getElementById("info").innerText = "NO";
+    var pass = document.getElementById('text1').value;
+    //alert("value is: " + str);
+    if (pass == "(1,1)" || pass == "(1, 1)" || pass == "1,1" || pass == "1, 1") {
+        window.location.href = "https://xroi.github.io/landing";
+    } else {
+        document.getElementById("info").innerHTML = "WRONG";
+    }
+
 }
